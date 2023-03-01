2a90f3dc9722f348193c55ee27434ae581a9029d
hondziq <hondziq@gmail.com>
2023-03-01 01:41:59 +0100

    added function removeCountry (task1/part3)

--- task1.js
+++ task1.js
@@ -59,3 +59,20 @@ function addCountry(country, countries) {
     );
   }
 }
+
+// PART3
+
+function removeCountry(country, countries) {
+  if (typeof country != "string" || country.length === 0) {
+    console.log(`Zadali jste neplatný dotaz: ${country}`);
+  } else if (countries.includes(country)) {
+    console.log(
+      `Zadaná krajina ${country} byla odstraněná se seznamu krajin na indexu ${countries.indexOf(
+        country
+      )}. Aktuální počet krajin v seznamu je ${countries.length - 1}.`
+    );
+    countries.splice(countries.indexOf(country), 1);
+  } else {
+    console.log(`Zadaná krajina ${country} se nenachází v seznamu.`);
+  }
+}
