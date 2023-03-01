f393ab3be5d1552e6eb7a1a23098818f247fe27d
hondziq <hondziq@gmail.com>
2023-03-01 03:18:34 +0100

    add task2.js

---
+++ task2.js
@@ -0,0 +1,14 @@
+const countriesPopulation = [
+  { country: "China", population: 1439323776 },
+  { country: "India", population: 1379302771 },
+  { country: "USA", population: 331002651 },
+  { country: "Indonesia", population: 269603400 },
+  { country: "Pakistan", population: 220892340 },
+  { country: "Bangladesh", population: 164970459 },
+  { country: "Japan", population: 126451398 },
+  { country: "Philippines", population: 109581078 },
+  { country: "Vietnam", population: 97338579 },
+  { country: "Turkey", population: 84339642 },
+];
+
+const populationTotal = 8000000000;
